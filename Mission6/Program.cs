9d1b51cd147a7f959f7b8c9241b5e21da8dcde98
ethan <ethanmalan10@gmail.com>
2026-02-14 09:43:01 -0700

    change

--- Mission6/Program.cs
+++ Mission6/Program.cs
@@ -1,7 +1,10 @@
+using Mission6.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<MovieRepository>();
 
 var app = builder.Build();
 
